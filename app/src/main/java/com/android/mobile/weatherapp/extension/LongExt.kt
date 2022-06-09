e52fe5b2087e53469d24c51fa98df44c86f4b408
mikkelseptiano <mikkels86@gmail.com>
2022-06-09 08:46:03 +0700

    fix next 3 days timestamp

--- app/src/main/java/com/android/mobile/weatherapp/extension/LongExt.kt
+++ app/src/main/java/com/android/mobile/weatherapp/extension/LongExt.kt
@@ -3,6 +3,6 @@ package com.android.mobile.weatherapp.extension
 import java.util.*
 
 fun Long.next3DaysTimeStamp() : Long {
-    val next3Days =  Date(this + (2 * (1000 * 60 * 60 * 24)))
-    return next3Days.time / 1000
+    val next3Days =  Date(this + (3 * (1000 * 60 * 60 * 24)))
+    return next3Days.time
 }
